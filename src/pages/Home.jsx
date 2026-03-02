55503534181df9606820e1e8acb75a83f5bb91cd
davyj93 <davyj93@gmail.com>
2026-03-02 13:32:39 +0000

    Replace static reviews with live widget

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,12 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import Services from '../components/Services'
 import WhyChooseUs from '../components/WhyChooseUs'
-import Testimonials from '../components/Testimonials'
 import FAQ from '../components/FAQ'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 import StickyCallButton from '../components/StickyCallButton'
 import BackToTop from '../components/BackToTop'
+import Reviews from '../components/Reviews'
 
 export default function Home() {
   return (
@@ -19,7 +19,7 @@ export default function Home() {
         <About />
         <Services />
         <WhyChooseUs />
-        <Testimonials />
+        <Reviews />
         <FAQ />
         <Contact />
         <Footer />
